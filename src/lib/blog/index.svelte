80172c57326e726e26ad2545eb1a57a3b5f9dabc
oscar <oscar@aroundwire.com>
2021-06-02 16:18:39 -0700

    update index html content

---
+++ src/lib/blog/index.svelte
@@ -0,0 +1,40 @@
+<div class="container">
+	<div class="row blogs">
+		<div class="col-md">
+			<div class="card" style="width: 18rem;">
+				<img src="img/1.webp" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="card" style="width: 18rem;">
+				<img src="img/1.webp" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="card" style="width: 18rem;">
+				<img src="img/1.webp" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.blogs{
+			margin-top: 20px;
+	}
+</style>
