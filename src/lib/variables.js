998ad10f8dc1e7f4b3760c8285cf94be4f8d550b
oscar <oscar@aroundwire.com>
2021-06-02 16:15:17 -0700

    added variable

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,4 +1,5 @@
 export const variables = {
 	apiPath: import.meta.env.VITE_API_PATH,
-	appName: import.meta.env.VITE_APP_NAME
+	appName: import.meta.env.VITE_APP_NAME,
+	appUrl: import.meta.env.VITE_APP_URL
 }
